d340b5de9fd1826864d1069d71b2a674e18a1d81
JohnnyYamanaka <yamanaka.johnny@outlook.com>
2021-01-10 23:43:50 -0300

    daily 01.10

---
+++ 2454.py
@@ -0,0 +1,16 @@
+'''
+2454
+https://www.urionlinejudge.com.br/judge/pt/problems/view/2454
+'''
+
+p_door, r_door = list(map(int, input().split()))
+
+if p_door == 0:
+    print('C')
+
+else:
+    if r_door == 0:
+        print('B')
+
+    else:
+        print('A')
